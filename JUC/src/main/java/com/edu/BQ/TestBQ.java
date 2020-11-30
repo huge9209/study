43dabe9667bd8e4555efc2588ee61509f0490d85
hunter <1481638917@qq.com>
2020-11-30 15:52:49 +0800

    juc学习

--- JUC/src/main/java/com/edu/BQ/TestBQ.java
+++ JUC/src/main/java/com/edu/BQ/TestBQ.java
@@ -1,5 +1,8 @@
 package com.edu.BQ;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @program: JUC
  * @description:
@@ -7,4 +10,85 @@ package com.edu.BQ;
  * @create: 2020-11-30 15:13
  */
 public class TestBQ {
+    public static void main(String[] args) throws InterruptedException {
+        test4();
+    }
+
+    /**
+     * 抛出异常
+     */
+    public static void test1(){
+        ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue<String>(3);
+        System.out.println(blockingQueue.add("1"));
+        System.out.println(blockingQueue.add("2"));
+        System.out.println(blockingQueue.add("3"));
+        //System.out.println(blockingQueue.add("4"));
+        //IllegalStateException: Queue full 抛出异常
+        System.out.println(blockingQueue.element());//查看首元素是谁
+
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+        System.out.println(blockingQueue.remove());
+        //System.out.println(blockingQueue.remove());
+        //java.util.NoSuchElementException 抛出异常
+
+    }
+
+    /**
+     * 有返回值，没有异常
+     */
+    public static void test2(){
+        ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue<String>(3);
+        System.out.println(blockingQueue.offer("1"));
+        System.out.println(blockingQueue.offer("2"));
+        System.out.println(blockingQueue.offer("3"));
+        System.out.println(blockingQueue.offer("4"));
+        // 不抛出异常 返回false
+
+        System.out.println(blockingQueue.peek());//检测队首元素
+
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        // 不抛出异常 返回null
+
+    }
+
+    /**
+     * 等待。阻塞（一直注释）
+     */
+    public static void test3() throws InterruptedException {
+        ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue<String>(3);
+
+        //一直阻塞
+        blockingQueue.put("1");
+        blockingQueue.put("2");
+        blockingQueue.put("3");
+        //blockingQueue.put("4");//队列没有位置了，一直阻塞
+        System.out.println(blockingQueue.take());
+        System.out.println(blockingQueue.take());
+        System.out.println(blockingQueue.take());
+        //System.out.println(blockingQueue.take());//没有元素 一直等待
+    }
+
+    /**
+     * 超时等待
+     */
+    public static void test4() throws InterruptedException {
+        ArrayBlockingQueue blockingQueue = new ArrayBlockingQueue<String>(3);
+
+
+        blockingQueue.offer("1");
+        blockingQueue.offer("2");
+        blockingQueue.offer("3");
+        blockingQueue.offer("4", 2,TimeUnit.SECONDS);//超过两秒就退出
+
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        System.out.println(blockingQueue.poll());
+        blockingQueue.poll(2,TimeUnit.SECONDS);
+
+    }
+
 }
---
+++ JUC/src/main/java/com/edu/pool/Demo1.java
@@ -0,0 +1,10 @@
+package com.edu.pool;
+
+/**
+ * @program: JUC
+ * @description:
+ * @author: lihangyu
+ * @create: 2020-11-30 15:52
+ */
+public class Demo1 {
+}
