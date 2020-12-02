79d9c13e80293905eaf0ac832c7c85ccb296a9eb
hunter <1481638917@qq.com>
2020-12-02 20:04:34 +0800

    复习反射

--- base-java/src/com/edu/refrect/Test3.java
+++ base-java/src/com/edu/refrect/Test3.java
@@ -1,5 +1,10 @@
 package com.edu.refrect;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 /**
  * @program: base-java
  * @description:
@@ -7,4 +12,29 @@ package com.edu.refrect;
  * @create: 2020-12-02 19:45
  */
 public class Test3 {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, NoSuchFieldException {
+        Class aClass = User.class;
+        User user = (User) aClass.newInstance();//本质调用了类得无参构造器
+        System.out.println(user);
+
+        System.out.println("---------------");
+        Constructor constructor = aClass.getDeclaredConstructor(String.class,String.class);
+        constructor.setAccessible(true);
+        User user1 = (User) constructor.newInstance("pianpian","18");
+        System.out.println(user1);
+
+        System.out.println("---------------");
+        User user2 = (User) aClass.newInstance();
+        Method method = aClass.getDeclaredMethod("setName",String.class);
+        method.setAccessible(true);
+        method.invoke(user2,"painpian1");
+        System.out.println(user2);
+
+        System.out.println("---------------");
+        User user3 = (User) aClass.newInstance();
+        Field name = aClass.getDeclaredField("name");
+        name.setAccessible(true);
+        name.set(user3,"pianpian2");
+        System.out.println(user3);
+    }
 }
--- base-java/src/com/edu/refrect/User.java
+++ base-java/src/com/edu/refrect/User.java
@@ -33,4 +33,12 @@ public class User {
     public void setAge(String age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
 }
